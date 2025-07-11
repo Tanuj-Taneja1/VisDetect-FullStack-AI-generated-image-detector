b97782d94df4e0e225f3032492c0fc58f2f93352
Tanuj Taneja <tanuj.taneja.1206@gmail.com>
2025-07-12 01:02:50 +0530

    Added className Prop to Logo component

--- visdetect/src/components/Logo/Logo.tsx
+++ visdetect/src/components/Logo/Logo.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-
-export default function Logo() {
+type LogoProp = { className?: string };
+export default function Logo({ className }: LogoProp) {
 	return (
 		<Image
 			src="/images/logo.png"
 			alt="VisDetect Logo"
 			width={1393}
 			height={287}
+			className={className}
 		/>
 	);
 }
