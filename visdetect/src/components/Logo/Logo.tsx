3ce2ea9d29dc99dcae9bff1cd2fa5d0161065f79
Tanuj Taneja <tanuj.taneja.1206@gmail.com>
2025-07-12 00:44:34 +0530

    Created Logo Server Component

---
+++ visdetect/src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Logo() {
+	return (
+		<Image
+			src="/images/logo.png"
+			alt="VisDetect Logo"
+			width={1393}
+			height={287}
+		/>
+	);
+}
