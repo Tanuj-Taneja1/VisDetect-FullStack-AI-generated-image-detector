667ee5229ac91bc9af6bf313c18be0daa4655b15
Tanuj Taneja <tanuj.taneja.1206@gmail.com>
2025-07-12 13:55:22 +0530

    Added Clerk Auth

--- visdetect/src/app/layout.tsx
+++ visdetect/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+
 export const metadata: Metadata = {
 	title: "VisDetect - Detect AI Generated Images",
 	description:
@@ -11,8 +13,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
-			<body>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
