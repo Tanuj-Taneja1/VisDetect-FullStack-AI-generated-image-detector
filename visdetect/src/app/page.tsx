115523343f395709083d4a80a5caa5539c9468cd
Tanuj Taneja <tanuj.taneja.1206@gmail.com>
2025-07-13 15:17:30 +0530

    Modified HomePage: added hero Banner

--- visdetect/src/app/page.tsx
+++ visdetect/src/app/page.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import HeroBanner from "@/components/HomePage/HeroBanner/HeroBanner";
 
 export default function Home() {
-	return <h1>Home Page</h1>;
+	return (
+		<>
+			<HeroBanner />;
+		</>
+	);
 }
